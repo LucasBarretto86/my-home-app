c3445759284d30bc83bb7f0aed0a2c1b0e4faae8
Lucas <mohamed28.lucas@gmail.com>
2021-05-02 01:56:57 -0300

    Update views, tests and few assets

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -1,10 +1,6 @@
 import {Controller} from 'stimulus'
 
 export default class extends Controller {
-    connect() {
-        console.log(this.element)
-    }
-
     close(){
         console.log("click")
         this.element.open = false
--- test/controllers/property_controller_test.rb
+++ test/controllers/property_controller_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
+require 'faker'
 class PropertyControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @property = Property.create name: Faker::Address.street_name
+  end
+
+  test 'that the action index redirects to the property main list' do
+    get properties_path
+    assert_response 200
+    assert_select '#properties-index'
+  end
+
+  test 'that the action show redirects to the property details' do
+    get property_path @property
+    assert_response 200
+    assert_select "#property_#{@property.id}-show"
+  end
 end
