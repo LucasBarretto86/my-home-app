b512b62f22a9bfb05c2800d0acac67d5b34cfcd5
Lucas <mohamed28.lucas@gmail.com>
2021-05-07 01:18:42 -0300

    Update project and fix n+1 problem for index action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
   def svg_inline(name)
     file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
 
-    return File.read(file_path).html_safe if File.exists?(file_path)
+    File.read(file_path).html_safe
   end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -7,4 +7,8 @@ class ApplicationHelperTest < ActionView::TestCase
   test 'That svg_inline method render svg correctly' do
     assert_match svg_inline(:home), '<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 372 418"><path d="M174.6 385.2L135.5 339.1H47.5V137.3L185.9 37.8 324.2 137.3V339.1H224.6" style="fill:none;stroke-linejoin:round;stroke-width:32;stroke:#007bff"/></svg>'
   end
+
+  test 'That svg_inline method raises exception when file is not found' do
+    assert_raises(Errno::ENOENT) { svg_inline(:nothing) }
+  end
 end
